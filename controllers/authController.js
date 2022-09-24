4b5c75132414b91efecc1bd16228cd9a3a6874b3
Niseme <nijolesemele@gmail.com>
2022-09-24 17:43:26 +0300

    bcrypt start

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,29 @@
-import userModel from "../models/user.js"
+import userModel from "../models/user.js";
+import bcrypt from "bcrypt";
 
-export const postRegistration = async(req,res) => {
-    const {userName,email,password} = req.body;
-    const existing = await userModel.findOne({email:email});
-    console.log(existing)
-    if(existing){
-        return res.status(403).json({message:"User already exist"});
-    }
-    const newUser = new userModel({userName,email,password});
-    const data = await newUser.save();
+export const postRegistration = async (req, res) => {
+  const { userName, email, password } = req.body;
+  const existing = await userModel.findOne({ email: email });
+  console.log(existing);
+  if (existing) {
+    return res.status(403).json({ message: "User already exist" });
+  }
+  const newUser = new userModel({ userName, email, password });
+  const data = await newUser.save();
 
-    res.send(data);
-}
+  res.send(data);
+};
 
-export const postLogin = async(req,res) => {
-    const {email,password} = req.body;
-    const existing = await userModel.findOne({email:email,password:password});
-    console.log(existing)
-    if(existing){
-        return res.status(200).json(existing);
-    }else{
-        return res.status(401).json({message:"Wrong credentials"});
-    }
-    
-}
-
-//test comment
+export const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const existing = await userModel.findOne({
+    email: email,
+    password: password,
+  });
+  console.log(existing);
+  if (existing) {
+    return res.status(200).json(existing);
+  } else {
+    return res.status(401).json({ message: "Wrong credentials" });
+  }
+};
